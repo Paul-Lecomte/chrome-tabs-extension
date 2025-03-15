13ad11eab4ad068942f9de841576df4e580980ac
Paul Lecomte <164857566+Paul-Lecomte@users.noreply.github.com>
2025-03-15 09:09:17 +0100

    log for context

--- public/background.js
+++ public/background.js
@@ -1,3 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
     console.log("Tab Organizer Extension Installed");
 });
+
+chrome.action.onClicked.addListener(() => {
+    console.log("Extension icon clicked");
+});
